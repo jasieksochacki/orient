73231ae65ed754e753c7a728aba0c583865ab136
Jan Sochacki <jasiek.sochacki1@gmail.com>
2019-05-10 12:18:24 +0200

    szablon zgłoszenia

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,2 +1,11 @@
 class Athlete < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :gender, presence: true
+  validates :birthdate, presence: true
+  validates :city, presence: true
+  validates :route_id, presence: true
+  validates :phone, presence: true
+  validates :team, presence: true
+  validates :email, presence: true
 end
