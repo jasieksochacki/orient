fb5428da74de7dceb31352f477758b54ba41bc42
Marcin Bajer <bajer@tigana.pl>
2019-05-15 10:27:34 +0200

    Set up ActiveAdmin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :athletes
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   root to: "home#index"
+
+  resources :athletes
 end
