5dab0f75621015de2f77ee0588f2c47a5de8ecf6
Jan Sochacki <jasiek.sochacki1@gmail.com>
2019-05-21 09:49:18 +0200

    Zapisy na więcej zawodów

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ApplicationRecord
-  validates :name, presence: true                                                                             
+  validates :name, presence: true
   validates :event_date, presence: true
   validates :last_entries_date, presence: true
 
   has_many :routes
   has_many :athletes
+
+  scope :active, -> { where("last_entries_date >= ?", Date.today) }
 end
